95e44de5f8c3e9318416f906a4b7d7bbe4e4a65f
liushuang <735385037@qq.com>
2019-07-09 22:00:36 +0800

    12

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @EnableEurekaServer
 @SpringBootApplication
+@EnableFeignClients
 public class DemoApplication {
 
 	public static void main(String[] args) {
